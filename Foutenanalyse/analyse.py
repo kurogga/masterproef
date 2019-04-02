89895262267b8acdd3ca74f0f0961ca332917efa
jtong <Jone.Tong@UGent.be>
2019-04-02 16:06:04 +0200

    Grafiek uitbreiding

--- Foutenanalyse/analyse.py
+++ Foutenanalyse/analyse.py
@@ -1,4 +1,3 @@
-
 # Fouten met PSNR en SSIM van 2 beelden berekenen en plotten
 # originele beelden worden opgeslagen in original-folder
 # changed-folder bevat gereconstrueerde beelden
@@ -13,9 +12,6 @@ import sys
 from skimage import img_as_float
 from skimage.measure import compare_ssim as ssim
 
-ssim_array = []
-psnr_array = []
-
 def mse(x, y):
     return np.linalg.norm(x - y)
 
@@ -29,51 +25,74 @@ def get_psnr(x, y):
     PIXEL_MAX = 255.0
     return 20 * math.log10(PIXEL_MAX / math.sqrt(_mse))
 
-def compare_images(x, y):
+def compare_images(x, y, ssim_array, psnr_array):
     original_file = cv2.imread(x)
     img = img_as_float(original_file)
     changed_file = cv2.imread(y)
     img_noise = img_as_float(changed_file)
-
-#    ssim_none = get_ssim(img, img)
-#    psnr_none = get_psnr(img,img)
     ssim_noise = get_ssim(img, img_noise)
-    psnr_noise = get_psnr(img,img_noise)
-
-#    ssim_array.append(ssim_none)
+    psnr_noise = get_psnr(img, img_noise)
     ssim_array.append(ssim_noise)
-#    psnr_array.append(psnr_none)
     psnr_array.append(psnr_noise)
 
-def plot_graph():
-    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 4),
-                         sharex=True, sharey=False)
+def plot_graph(n, ssim_arrays, psnr_arrays, labels):
+    fig, axes = plt.subplots(
+        nrows=1, ncols=2, figsize=(10, 4), sharex=True, sharey=False
+    )
     ax = axes.ravel()
-    x_max = len(ssim_array)
-    ax[0].plot(ssim_array)
-    ax[0].set_ylabel('SSIM [%]')
-    ax[0].set_xlabel('frame number')
-    ax[0].set_title('Structural similarity')
-    ax[0].set_ylim([0.0,1.0])
-    ax[0].set_xlim([0,x_max+5])
-    
-    ax[1].plot(psnr_array)
-    ax[1].set_ylabel('PSNR [dB]')
-    ax[1].set_xlabel('frame number')
-    ax[1].set_title('Peak signal-to-noise ratio')
-    ax[1].set_ylim([10,60])
+    x_max = len(ssim_arrays[0])
+    for x in range(0,len(ssim_arrays)):
+        ax[0].plot(ssim_arrays[x], label=labels[x])
+    ax[0].set_ylabel("SSIM [%]")
+    ax[0].set_xlabel("frame number")
+    ax[0].set_title("Structural similarity")
+    ax[0].set_ylim([0.0, 1.0])
+    ax[0].set_xlim([0, x_max])
+    ax[0].legend(loc='best')
+    ax[0].grid(True)
+
+    for x in range(0,len(psnr_arrays)):
+        ax[1].plot(psnr_arrays[x], label=labels[x])
+    ax[1].set_ylabel("PSNR [dB]")
+    ax[1].set_xlabel("frame number")
+    ax[1].set_title("Peak signal-to-noise ratio")
+    ax[1].set_ylim([10, 60])
+    ax[1].legend(loc='best')
+    ax[1].grid(True)
     
     plt.tight_layout()
     plt.show()
- 
-files = next(os.walk("original"))[2]
-files2 = next(os.walk("changed"))[2]
 
-if len(files)==0 and len(files)!=len(files2):
-    sys.exit("Error: not enough files!")
+path_original = "original/"
+path_changed = "changed/"
 
-for x in range(1,len(files)+1):    
-    compare_images("original/"+str(x)+".png","changed/"+str(x)+".png")
+# for comparison of 2 or more folders of images 
+if len(sys.argv) > 2:
+    path_original = sys.argv[1] + "/"
+    files = next(os.walk(path_original))[2]
     
-plot_graph()
+    if len(files) == 0:
+        sys.exit("Error: not enough files!")
+    
+    ssim_arrays = []
+    psnr_arrays = []    
+    labels = []
+    for x in range(2,len(sys.argv)):
+        labels.append(sys.argv[x])
+        path_changed = sys.argv[x] + "/"
+        files2 = next(os.walk(path_changed))[2]
+        amount = min(len(files),len(files2))
+        ssim_array = []
+        psnr_array = []
+        ssim_arrays.append(ssim_array)
+        psnr_arrays.append(psnr_array)
+        
+        for y in range(0, amount):
+            compare_images(path_original + files[y], path_changed + files2[y],ssim_arrays[x-2],psnr_arrays[x-2])
+            
+    plot_graph(x-2,ssim_arrays,psnr_arrays,labels)
+    
+else:
+    sys.exit("Error: please enter at least 2 folder names containing the images with the original (comparator) folder first!")
+
 
--- README.md
+++ README.md
@@ -3,10 +3,15 @@ Rendering light field images on mobile devices
 
 ## Getting Started
 ### Foutenanalyse
-Script neemt telkens een beeld uit orignal-folder en een uit changed-folder om te vergelijken. PSNR- en SSIM-waarden worden berekend en op grafiek geplot.
+Script neemt telkens een beeld uit orignal-folder en een uit changed-folder om te vergelijken. PSNR- en SSIM-waarden worden berekend en op grafiek geplot. 
+Via commandolijn oproepen: 
+> python analyse.py *originalfolder testfolder1 testfolder2 ...*
 
 ### OpenGL_Project 
 Het project bevat een testprogramma met geometry instancing in OpenGL in c++.
 
 ### Unity_Shadertoy
 Het project is een omzetting van shadertoy project naar Unity. Alle shaders zitten in [Assets/Shaders](https://github.com/kurogga/masterproef/tree/master/Unity_Shadertoy/Assets/Shaders). De gewenste shader downloaden en toevoegen in eigen projects asset is voldoende.
+
+### Unity_LightFieldRecon
+Dit bevat de reconstructie van SMoE in Unity. De C#-script en shader zitten in de Asset-folder. Dus enkel die folder importeren in eigen project is genoeg. Binnen het project hoeft er zoals in Shadertoy enkel een quad-object voor de camera te hebben en de juiste script en shader op het object toepassen. Gebouwde applicatie is ook beschikbaar in de Build-folder.
